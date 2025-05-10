55a7dfdeffaa53d03520d7b29ceda010510bd47f
wangyanli02 <wangyanli02@daojia-inc.com>
2025-05-10 17:33:24 +0800

    feat: add

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,9 +3,6 @@ import { electronAPI } from '@electron-toolkit/preload'
 
 const api = {}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)

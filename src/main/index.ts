8381c5b697cbf8c7b9749f37ddf482ce5410f3bb
wangyanli02 <wangyanli02@daojia-inc.com>
2025-05-12 14:42:41 +0800

    feat: 主进程

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,7 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -25,8 +26,11 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+  // console.log('process.env: ', process.env)
+  console.log('is--: ', is) // { dev: true }
+
+  // 基于 electron-vite CLI 的渲染进程热模块替换（HMR）。
+  // 在开发环境中加载远程 URL，在生产环境中加载本地 HTML 文件。https://cn.electron-vite.org/guide/hmr
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -34,13 +38,15 @@ function createWindow(): void {
   }
 }
 
+// 当 Electron 完成初始化并准备好创建浏览器窗口时会调用此方法。
+// 一些 API 只能在此事件发生后才能使用
 app.whenReady().then(() => {
-  // Set app user model id for windows
+  // 为 Windows 设置应用程序用户模型 ID
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  // 在开发环境中按 F12 默认打开或关闭开发者工具
+  // 在生产环境中忽略 CommandOrControl + R 快捷键
+  // 参见：https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
@@ -51,12 +57,19 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
+    // 在 macOS 上，当点击 Dock 图标且没有其他窗口打开时，
+    // 通常会重新创建一个应用窗口。
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+// 当所有窗口都被关闭时退出应用，macOS 除外。
+// 在 macOS 上，通常应用及其菜单栏会保持激活状态，直到用户使用 Cmd + Q 显式退出。
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+// 在这个文件中，你可以包含应用特定的主进程代码的其余部分。
+// 你也可以将它们放在单独的文件中，并在这里引入

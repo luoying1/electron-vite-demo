103f108e105c8a7bbee022c83c7da06bc98e9485
wangyanli02 <wangyanli02@daojia-inc.com>
2025-05-12 14:56:48 +0800

    feat: 预加载

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,12 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
+// Custom APIs for renderer
 const api = {}
 
+// 仅当启用了上下文隔离（context isolation）时，
+// 使用 `contextBridge` API 将 Electron 的 API 暴露给渲染进程；
+// 否则，直接添加到全局 DOM 中即可。
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
